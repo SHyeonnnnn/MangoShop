ef668a03632e0d06197cf1a1e6759820adfd808c
SHyeonnnnn <dnsdkatmdgus@naver.com>
2023-02-10 13:03:42 +0900

    슬러시삭제

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 //고정 변수
-export const API_URL=process.env.NODE_ENV === 'production' ? "https://port-0-mangoserver-3a9t2bldxxhouu.sel3.cloudtype.app/" : "http://localhost:8080";
+export const API_URL=process.env.NODE_ENV === 'production' ? "https://port-0-mangoserver-3a9t2bldxxhouu.sel3.cloudtype.app" : "http://localhost:8080";
